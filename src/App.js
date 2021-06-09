c6276979701fdb21867bd5abaa01ce4d4e0cf52a
Paul Mackay <paul.m.mackay85@gmail.com>
2021-06-09 16:25:11 +0100

    Managing State

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import GoalList from './components/GoalList/GoalList';
 import NewGoal from './components/NewGoal/NewGoal';
 import './App.css';
 
 const App = () => {
-  const courseGoals = [
+  const [courseGoals, setCourseGoals] = useState([
     {id: 'cg1', text: 'Finish the Course'},
     {id: 'cg2', text: 'Learn all about the Course Main Topic'},
     {id: 'cg3', text: 'Help other students in the Course Q&A'}
-  ]; 
+  ]); 
 
   // return React.createElement('h1', {}, 'Hi, this is React!');
   // return <h1 title="This works!">Hi, this is ReactJS!</h1>;
   // return React.createElement('h1', {title: 'This works!'}, 'Hi, this is ReactJS!');
   
   const addNewGoalHandler = (newGoal) => {
-    courseGoals.push(newGoal);
-    console.log(courseGoals);
+    setCourseGoals(courseGoals.concat(newGoal));
   };
 
   return (
