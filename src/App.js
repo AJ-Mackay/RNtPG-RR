7fefbd0c0db921278dd42cbb4957588c37b2a8b2
Paul Mackay <paul.m.mackay85@gmail.com>
2021-06-09 14:14:50 +0100

    Working with Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import GoalList from './components/GoalList';
+import './App.css';
+
 const App = () => {
   // return React.createElement('h1', {}, 'Hi, this is React!');
-  return <h1 title="This works!">Hi, this is ReactJS!</h1>;
+  // return <h1 title="This works!">Hi, this is ReactJS!</h1>;
   // return React.createElement('h1', {title: 'This works!'}, 'Hi, this is ReactJS!');
+  return (
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <GoalList />
+    </div>
+  );
 };
 
 // Old React would be written as:
