08ed364ff74343904fe5a81f7243e64dbb085bd3
Paul Mackay <paul.m.mackay85@gmail.com>
2021-06-09 15:57:43 +0100

    Working with Props

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import GoalList from './components/GoalList';
 import './App.css';
 
 const App = () => {
+  const courseGoals = [
+    {id: 'cg1', text: 'Finish the Course'},
+    {id: 'cg2', text: 'Learn all about the Course Main Topic'},
+    {id: 'cg3', text: 'Help other students in the Course Q&A'}
+  ]; 
+
   // return React.createElement('h1', {}, 'Hi, this is React!');
   // return <h1 title="This works!">Hi, this is ReactJS!</h1>;
   // return React.createElement('h1', {title: 'This works!'}, 'Hi, this is ReactJS!');
+  
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <GoalList />
+      <GoalList goals={courseGoals} />
     </div>
   );
 };
